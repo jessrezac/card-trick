a1404c58f6482a08e976b6abc621574264e5a4d1
Jess Rezac <jess.rezac@gmail.com>
2019-10-28 20:21:39 -0500

    generate User resource

---
+++ db/migrate/20191029012049_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :password_digest
+      t.string :google_token
+      t.string :google_refresh_token
+
+      t.timestamps
+    end
+  end
+end
