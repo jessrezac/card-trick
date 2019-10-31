f0a813c5d5556ea6d9c7365abcbd5db07d7f120b
Jess Rezac <jess.rezac@gmail.com>
2019-10-30 22:50:41 -0500

    set root and add to nav

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get "/", to: "welcome#home"
+  root to: "welcome#home"
 
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
 
   get "/login", to: "sessions#new"
   post "/logout", to: "sessions#destroy"
+  
 end
