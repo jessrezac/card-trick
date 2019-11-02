782a87cc8698cafcfd8113d3673dcdaeaba1bf7f
Jess Rezac <jess.rezac@gmail.com>
2019-11-02 17:22:33 -0500

    add columns to Cards table, run migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_31_114924) do
+ActiveRecord::Schema.define(version: 2019_11_02_171502) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -31,6 +31,17 @@ ActiveRecord::Schema.define(version: 2019_10_31_114924) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "cards", force: :cascade do |t|
+    t.text "front_content"
+    t.string "front_image"
+    t.text "back_content"
+    t.string "back_image"
+    t.integer "mastery"
+    t.integer "stack_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "stacks", force: :cascade do |t|
