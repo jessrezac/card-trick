19a3376fa6fab2157ca7e486cef0007f427a78f0
Jess Rezac <jess.rezac@gmail.com>
2019-11-08 14:40:56 -0600

    create imports#create action

--- app/controllers/imports_controller.rb
+++ app/controllers/imports_controller.rb
@@ -1,3 +1,6 @@
+require 'google/apis/drive_v3'
+require 'google/api_client/client_secrets.rb'
+
 class ImportsController < ApplicationController
 
     def new
@@ -5,13 +8,39 @@ class ImportsController < ApplicationController
     end
 
     def create
-        raise import_params.inspect
+        @import = Import.new(import_params)
+        @import.authorize(google_secret)
+        @import.request_comments
+
+        @stack = Stack.new
+        @import.comments.each do |comment|
+            
+        end
+
+        @list = comment_list.comments.map { |comment| "#{comment.content} - #{comment.quoted_file_content.value if comment.quoted_file_content }"}
+
+        render "stacks/new"
+
     end
 
     private
 
     def import_params
         params.require(:import).permit(:file_id)
+    end
+
+    def google_secret
+        Google::APIClient::ClientSecrets.new(
+            {
+                "web" =>
+                {
+                    "access_token" => current_user.google_token,
+                    "refresh_token" => current_user.google_refresh_token,
+                    "client_id" => ENV['GOOGLE_CLIENT_ID'],
+                    "client_secret" => ENV['GOOGLE_CLIENT_SECRET']
+                }
+            }
+        )
     end
 
 end
