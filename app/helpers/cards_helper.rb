51e66bd6a2cc1b09816f6a6eeb55ab8910f7e6f7
Jess Rezac <jess.rezac@gmail.com>
2019-11-09 13:42:58 -0600

    create skip card view helper

--- app/helpers/cards_helper.rb
+++ app/helpers/cards_helper.rb
@@ -1,2 +1,11 @@
 module CardsHelper
+
+    def skip_card
+        if @card.next
+            link_to "Skip", stack_card_path(@card.stack, @card.next), class: "button is-large"
+        else
+            link_to "Finish", stack_path(@card.stack), class: "button is-large"
+        end
+    end
+    
 end
