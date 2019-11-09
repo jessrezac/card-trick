20efcb6fc96ac1135b61b9c8c49bff57bae7b8a5
Jess Rezac <jess.rezac@gmail.com>
2019-11-08 19:58:45 -0600

    advance through cards in stack

--- app/models/card.rb
+++ app/models/card.rb
@@ -2,4 +2,12 @@ class Card < ApplicationRecord
     belongs_to :stack
     has_one_attached :front_image
     has_one_attached :back_image
+
+    def next
+        Card.where('stack_id = ?', self.stack_id).where('id > ?', self.id).first
+    end
+
+    def previous
+        Card.where('stack_id = ?', self.stack_id).where('id < ?', self.id).last
+    end
 end
